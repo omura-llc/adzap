5312f712828bdd5ea4d0412d0b2b3b01560dc7bb
Omura LLC <17105749+omura-llc@users.noreply.github.com>
2024-04-20 00:27:06 -0700

    Add HelpScreen

---
+++ App/HelpScreen.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+struct HelpScreen: View {
+    var buttonColor: Color
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text(LocalizedStringKey("help-text"))
+                    .padding()
+
+                Link(destination: URL(string: "https://github.com/omura-llc/adzap/issues")!) {
+                    Text(LocalizedStringKey("github-issue"))
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(buttonColor)
+                        .cornerRadius(5)
+                }
+                .frame(maxWidth: .infinity)
+                .multilineTextAlignment(.center)
+
+                Text(versionBuildText)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+        }
+    }
+
+    private var versionBuildText: String {
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+        let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
+        return "version \(version) (\(build))"
+    }
+}
+
+struct HelpScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            HelpScreen(buttonColor: .bannerColorOmura).preferredColorScheme(.light)
+            HelpScreen(buttonColor: .bannerColorGrape).preferredColorScheme(.dark)
+        }
+        .previewLayout(.device)
+    }
+}
