b0dad992cd483dcddb868ee307e0c08e8f88926e
Omura LLC <17105749+omura-llc@users.noreply.github.com>
2024-04-20 00:27:29 -0700

    Update HomeScreen

--- App/HomeScreen.swift
+++ App/HomeScreen.swift
@@ -1,78 +1,70 @@
 import SwiftUI
-import UIKit
-
-func goodVibes() {
-    let generator = UIImpactFeedbackGenerator(style: .heavy)
-    generator.prepare()
-    generator.impactOccurred(intensity: 1.0)
-}
 
 struct HomeScreen: View {
     var isEnabled: Bool
     @State private var colorIndex = 0
+    @State private var showHelp = false
     
-    private let logoColors = [Color.logoColorClover, Color.logoColorCherry, Color.logoColorOmura, Color.logoColorTangerine, Color.logoColorGrape, Color.logoColorSunshine]
+    private let bannerColors = [Color.bannerColorClover, Color.bannerColorOmura, Color.bannerColorGrape, Color.bannerColorTangerine]
     
-    private var version: String {
-        Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
+    private var versionBuildText: String {
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "N/A"
+        let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "N/A"
+        return "\n\(version) (\(build))"
     }
-    
-    private var build: String {
-        Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? ""
-    }
-    
+
     var body: some View {
-        GeometryReader { proxy in
-            VStack {
-                Logo(enabled: isEnabled, logoColor: logoColors[colorIndex])
-                    .padding(.top, 100)
-                    .padding(.horizontal, 20)
-                    .frame(height: proxy.size.height / 1.5)
-                    .gesture(
-                        DragGesture()
-                            .onEnded { gesture in
-                                if isEnabled {
-                                    let horizontalMovement = gesture.translation.width
-                                    let swipeDistance: CGFloat = 200
-                                    if abs(horizontalMovement) > swipeDistance {
-                                        colorIndex = (colorIndex + 1) % logoColors.count
-                                        goodVibes()
+        NavigationView {
+            GeometryReader { proxy in
+                VStack {
+                    Banner(enabled: isEnabled, bannerColor: bannerColors[colorIndex])
+                        .padding(.top, 100)
+                        .padding(.horizontal, 20)
+                        .frame(height: proxy.size.height / 1.5)
+                        .gesture(
+                            DragGesture()
+                                .onEnded { gesture in
+                                    if isEnabled {
+                                        let horizontalMovement = gesture.translation.width
+                                        let swipeDistance: CGFloat = 200
+                                        if abs(horizontalMovement) > swipeDistance {
+                                            colorIndex = (colorIndex + 1) % bannerColors.count
+                                        }
                                     }
                                 }
-                            }
-                    )
-
-                Spacer()
-                
-                Text(isEnabled ? "instructions-enabled" : "instructions-disabled")
-                    .font(.system(size: 17))
-                    .multilineTextAlignment(.center)
-                    .padding(.top)
-                
-                if isEnabled {
+                        )
+                    
                     Spacer()
-                } else {
-                    Text("Open Settings")
-                        .foregroundColor(.blue)
-                        .onTapGesture {
-                            if let url = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) {
-                                UIApplication.shared.open(url)
-                            }
+                    
+                    if isEnabled {
+                        Button(action: { showHelp = true }) {
+                            Image(systemName: "questionmark.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundColor(bannerColors[colorIndex])
                         }
-                        .font(.system(size: 17))
-                        .multilineTextAlignment(.center)
-                        .padding(.vertical)
+                    } else {
+                        Text(LocalizedStringKey("instructions"))
+                            .multilineTextAlignment(.center)
+                            .padding(.top)
+                        Text(LocalizedStringKey("open-settings"))
+                            .foregroundColor(.blue)
+                            .onTapGesture {
+                                if let url = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) {
+                                    UIApplication.shared.open(url)
+                                }
+                            }
+                            .multilineTextAlignment(.center)
+                            .padding(.vertical)
+                    }
+                    
+                    Spacer()
                 }
-
-                Text("\(version) (\(build))")
-                    .font(.system(size: 16))
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom)
-                
-                Spacer()
+                .overlay(
+                    NavigationLink(destination: HelpScreen(buttonColor: bannerColors[colorIndex]), isActive: $showHelp) { EmptyView() }
+                )
             }
         }
-        .ignoresSafeArea()
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -87,10 +79,6 @@ struct HomeScreen_Previews: PreviewProvider {
                 .preferredColorScheme(.dark)
             HomeScreen(isEnabled: true)
                 .preferredColorScheme(.dark)
-
-            HomeScreen(isEnabled: true)
-                .preferredColorScheme(.light)
-                .previewDevice("iPhone 15")
         }
         .previewLayout(.device)
     }
